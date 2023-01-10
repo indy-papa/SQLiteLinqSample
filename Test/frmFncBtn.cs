f41e1546afeca10dd4349bf86de30ff01babda18
Ikuma Uneno <85654475+indy-papa@users.noreply.github.com>
2023-01-11 00:11:24 +0900

    画面設計のためのフォーム追加

---
+++ Test/frmFncBtn.cs
@@ -0,0 +1,15 @@
+namespace Test
+{
+    public partial class frmFncBtn : Form
+    {
+        public frmFncBtn()
+        {
+            InitializeComponent();
+        }
+
+        private void frmFncBtn_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
